03cf4c9bd4e9848fa076cb844c9d9fa92b4a1254
Abhiram Ghanta <abhiramg2021@gmail.com>
2022-12-18 16:12:13 -0500

    basic wipe animation

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,22 +1,17 @@
+import "../style/index.css";
+
 export const MenuItem = (props) => {
   console.log(props.id == props.menuItemIndex);
   return (
     <div>
-      {props.id == props.menuItemIndex ? (
-        <span
-          className="transition cursor-pointer border-b-4 border-red-400"
-          onClick={() => props.setMenuItemIndex(props.id)}
-        >
-          {props.text}
-        </span>
-      ) : (
-        <span
-          className="transition cursor-pointer border-b-4 border-gray-300 hover:border-red-400"
-          onClick={() => props.setMenuItemIndex(props.id)}
-        >
-          {props.text}
-        </span>
-      )}
+      <span
+        className={`wipe-underline-${
+          props.id == props.menuItemIndex ? "highlight" : "animation"
+        } cursor-pointer pb-1`}
+        onClick={() => props.setMenuItemIndex(props.id)}
+      >
+        {props.text}
+      </span>
     </div>
   );
 };
