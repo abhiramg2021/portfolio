39c1d520b903c2ef18e4c22d508b064aff20bbff
Abhiram Ghanta <abhi@Abhirams-MacBook-Pro.local>
2022-12-18 10:45:59 -0500

    basic corners

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { InfoIcons } from "./components/InfoIcons";
 import { Landing } from "./components/Landing";
 import { ProjectsMenu } from "./components/ProjectsMenu";
+import "./style/index.css";
 
 function App() {
   return (
-    <div className=" flex h-screen w-screen justify-center bg-beige-200">
-      <div className="flex max-w-screen-lg flex-col items-center p-10">
+    <div className="relative h-screen w-screen overflow-x-hidden bg-beige-200">
+      <div className="borderLeft absolute top-0" />
+      <div className="borderTop absolute left-0" />
+      <div className="borderRight absolute bottom-0 right-0" />
+      <div className="borderBottom absolute right-0 bottom-0" />
+      <div className=" p-10">
         <InfoIcons />
         <div className="lg:flex-no-wrap flex flex-wrap items-center p-10">
           <Landing />
