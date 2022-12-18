1ba32a3cb80377ea40982452c7d1fcf755e70552
Abhiram Ghanta <abhi@Abhirams-MacBook-Pro.local>
2022-12-17 23:09:40 -0500

    more landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,9 @@
 export const Landing = () => {
   return (
     <div className="p-10">
-      <h1 className=" font-yellowtail text-l">Hello</h1>
+      <h1 className=" font-yellowtail text-xl">Hello</h1>
 
-      <div className="w-1/2">
+      <div className="m-0 w-1/2 border-l-8 bg-beige-600 pl-5">
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
